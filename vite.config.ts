7603044616410e82cc67871f5e3d2b10fcb9aa90
ehdwo99-beep <ehdwo99@gmail.com>
2026-02-20 23:54:15 +0900

    Update vite.config.ts
    base 주소 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/ipad-test/",
+  base: "/ipad-test/ipad-test/",
   server: {
     host: "::",
     port: 8080,
